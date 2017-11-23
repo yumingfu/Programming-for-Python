1fec555f9fae22846d1512dd6a789309f9140a4d
yumingfu <yuming.fu2@mail.dcu.oe>
2017-11-23 00:43:55 +0000

    menu design done

---
+++ menu.py
@@ -0,0 +1,30 @@
+def printLine (text = ''):
+    print('X {: ^47} X'.format(text))
+
+def printSelTitle (text = '', title = None):
+    print('X{: ^9}'.format(text), end = ""),
+
+def printSelCheck (text = '', title = None):
+    print('X   [ ]   '.format(text), end = "")
+
+def printBorder (title = None):
+    print('X' * 51)
+
+def printBox (*lines, title = None, showBottomBorder = False):
+    printBorder(title)
+    printLine()
+    for line in lines:
+        printLine(line)
+    printLine()
+    if showBottomBorder:
+        printBorder()
+
+printBox('Stock Analysis - Choose your stock')
+printBox('Are you ready to start?', '-' * 21,\
+         'Press any key to continue', '-' * 21, showBottomBorder = True)
+printSelTitle('TASK1'), printSelTitle('TASK2'),\
+        printSelTitle('TASK3'), printSelTitle('TASK4'),\
+        printSelTitle('TASK5'), print("X")
+printSelCheck(), printSelCheck(), printSelCheck(),\
+                 printSelCheck(), printSelCheck(), print("X")
+printBorder()
